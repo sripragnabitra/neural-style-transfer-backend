d54d33d1dc8129721080dfe153738c2ced639ee7
Sri Pragna Bitra <sripragnabitra@gmail.com>
2025-08-09 18:14:40 +0530

    CORS issue

--- app.py
+++ app.py
@@ -7,10 +7,16 @@ import traceback
 from style_transfer import stylize_bytes
 
 app = Flask(__name__)
-CORS(app, origins=[
-    "http://localhost:3000",
-    "https://neural-style-transfer-two.vercel.app"
-    ])
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "http://localhost:3000",
+            "https://neural-style-transfer-two.vercel.app"
+        ],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"],
+    }
+})
 
 @app.route('/', methods=['GET'])
 def home():
