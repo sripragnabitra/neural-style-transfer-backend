ade609c5bd7c6f700be818a5a3bf557ca9090474
Sri Pragna Bitra <sripragnabitra@gmail.com>
2025-08-09 17:22:47 +0530

    Procfile change

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def stylize():
         return jsonify({'error': str(e), 'trace': tb}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
